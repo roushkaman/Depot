d7080143dc28c45623cb927cdd75723bdf2fa7b4
Richec <roushenzo@gmail.com>
2012-09-20 22:16:56 +0300

    Validation and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  validates :title, uniqueness: true
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01} 
+  validates :image_url, allow_blank: true, format: {
+  	with: %r{\.(gif|jpg|png)$}i,
+  	message: 'format this: jpg, png and gif'
+  }
 end
