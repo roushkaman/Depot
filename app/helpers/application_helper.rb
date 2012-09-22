a757a1dc948f926b1de2b637c37a3e36235ae83b
Richec <roushenzo@gmail.com>
2012-09-22 08:43:55 +0300

    to create the new controller Store

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def time
+		Time.now
+	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
-  validates :title, :length => { maximum: 10 }, uniqueness: true
+  validates :title, :length => { maximum: 25, message: "Dlinna ne ta =). Nughno do 25" }, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01} 
   validates :image_url, allow_blank: true, format: {
